9de836109f2972bde0bfd85b7d5128f01edaf408
MinChan Lee <shigatsu970704@gmail.com>
2021-08-22 09:49:04 +0900

    [Feat] Create Room Form Input Context

--- src/components/pages/LoginHome/LoginHomeContainer.tsx
+++ src/components/pages/LoginHome/LoginHomeContainer.tsx
@@ -1,12 +1,80 @@
-import React, { useEffect } from 'react';
+import { yupResolver } from '@hookform/resolvers/yup';
+import React, { createContext, useContext, useEffect } from 'react';
+import {
+  Control,
+  DeepMap,
+  FieldError,
+  useForm,
+  UseFormHandleSubmit,
+  UseFormRegister,
+} from 'react-hook-form';
+import * as yup from 'yup';
 
 import socket from '@/socket';
 import { LoginHomePresenter } from '@pages/LoginHome/LoginHomePresenter';
 
+// Create Room Context Interface
+interface ICreateRoomFormContext {
+  register: UseFormRegister<CreateRoomFormInput>;
+  handleSubmit: UseFormHandleSubmit<CreateRoomFormInput>;
+  control: Control<CreateRoomFormInput>;
+  onValid: () => void;
+  errors: DeepMap<CreateRoomFormInput, FieldError>;
+  isValid: boolean;
+}
+
+// Create Room Form Input Interface
+interface CreateRoomFormInput {
+  title: string;
+  totalHeadCount: string;
+}
+
+// Login Validate Schema
+const createRoomSchema = yup.object().shape({
+  title: yup.string().required(),
+  totalHeadCount: yup.string().required(),
+});
+
+// Login Form Context 생성
+const CreateRoomFormContext = createContext<ICreateRoomFormContext | undefined>(undefined);
+
+export const useCreateRoomFormContext = () => {
+  const context = useContext(CreateRoomFormContext);
+  if (!context) throw new Error('Form Context가 존재하지 않습니다');
+  return context;
+};
+
 export const LoginHomeContainer: React.FC = () => {
+  const {
+    register,
+    handleSubmit,
+    control,
+    getValues,
+    formState: { errors, isValid },
+    reset,
+  } = useForm<CreateRoomFormInput>({ mode: 'all', resolver: yupResolver(createRoomSchema) });
+
+  const value = {
+    register,
+    handleSubmit,
+    control,
+    onValid,
+    errors,
+    isValid,
+  };
+
+  function onValid() {
+    console.log(getValues());
+    reset({ title: '', totalHeadCount: '4' });
+  }
+
   useEffect(() => {
     socket.emit('rooms:test:server', 'rooms');
   }, []);
 
-  return <LoginHomePresenter />;
+  return (
+    <CreateRoomFormContext.Provider value={value}>
+      <LoginHomePresenter />
+    </CreateRoomFormContext.Provider>
+  );
 };
