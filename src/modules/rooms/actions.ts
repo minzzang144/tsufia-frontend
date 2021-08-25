7eeb4c32283c64e80fb2b28bae8d4e6806ac2ee2
MinChan Lee <shigatsu970704@gmail.com>
2021-08-25 09:53:05 +0900

    [Rename] ADD_ROOM, ENTER_ROOM to ADD_ROOMS, ENTER_ROOMS Method

--- src/modules/rooms/actions.ts
+++ src/modules/rooms/actions.ts
@@ -5,13 +5,13 @@ import { Room } from '@rooms/types';
 export const UPDATE_ROOMS_LOADING = 'rooms/UPDATE_ROOMS_LOADING';
 export const UPDATE_ROOMS_ERROR = 'rooms/UPDATE_ROOMS_ERROR';
 export const GET_ROOMS = 'rooms/GET_ROOMS';
-export const ADD_ROOM = 'rooms/ADD_ROOM';
+export const ADD_ROOMS = 'rooms/ADD_ROOMS';
 export const UPDATE_ROOMS = 'rooms/UPDATE_ROOMS';
-export const ENTER_ROOM = 'rooms/ENTER_ROOM';
+export const ENTER_ROOMS = 'rooms/ENTER_ROOMS';
 
 export const updateRoomsLoading = createAction(UPDATE_ROOMS_LOADING)();
 export const updateRoomsError = createAction(UPDATE_ROOMS_ERROR)<string | undefined>();
 export const getRooms = createAction(GET_ROOMS)<Room[]>();
-export const addRoom = createAction(ADD_ROOM)<Room>();
+export const addRooms = createAction(ADD_ROOMS)<Room>();
 export const updateRooms = createAction(UPDATE_ROOMS)<Room>();
-export const enterRoom = createAction(ENTER_ROOM)<Room>();
+export const enterRooms = createAction(ENTER_ROOMS)<Room>();
