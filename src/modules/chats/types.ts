1ff088b427302a46c0c74f73e25d969ec5fbbf80
MinChan Lee <shigatsu970704@gmail.com>
2021-09-28 10:03:51 +0900

    [Set] Cycle Field on Chat Module

--- src/modules/chats/types.ts
+++ src/modules/chats/types.ts
@@ -3,6 +3,7 @@ import { ActionType } from 'typesafe-actions';
 import * as chatsAction from '@chats/actions';
 
 import { User } from '@auth';
+import { Cycle } from '@game';
 import { Room } from '@rooms';
 
 export type ChatsAction = ActionType<typeof chatsAction>;
@@ -12,6 +13,7 @@ export type Chat = {
   createdAt: Date;
   updatedAt: Date;
   content: string;
+  cycle: Cycle | null;
   room: Room;
   user: User;
 };
