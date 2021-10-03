7768cb0bc15c80ae3e3d6831bceafd4b6728f87f
MinChan Lee <shigatsu970704@gmail.com>
2021-10-03 11:35:22 +0900

    [Add] Validate Context on Validate Page Component

--- src/components/pages/ValidatePage/ValidatePageContainer.tsx
+++ src/components/pages/ValidatePage/ValidatePageContainer.tsx
@@ -1,7 +1,54 @@
-import React from 'react';
+import { yupResolver } from '@hookform/resolvers/yup';
+import React, { createContext, useContext } from 'react';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+
+import * as I from '.';
 
 import { ValidatePagePresenter } from '@pages/ValidatePage/ValidatePagePresenter';
 
+// Validate Context 생성
+const ValidateContext = createContext<I.IValidateContext | undefined>(undefined);
+
+// Validate Context Hook
+export const useValidateContext = () => {
+  const context = useContext(ValidateContext);
+  if (!context) throw new Error('Validate Context가 존재하지 않습니다');
+  return context;
+};
+
+const validatePasswordSchema = yup.object().shape({
+  password: yup.string().required(),
+});
+
 export const ValidatePageContainer: React.FC = () => {
-  return <ValidatePagePresenter />;
+  const {
+    register,
+    handleSubmit,
+    control,
+    getValues,
+    formState: { errors, isValid },
+    reset,
+  } = useForm<I.ValidateFormInput>({ mode: 'all', resolver: yupResolver(validatePasswordSchema) });
+
+  function onValid() {
+    const values = getValues();
+    console.log(values);
+    reset({ password: '' });
+  }
+
+  const value = {
+    register,
+    handleSubmit,
+    control,
+    onValid,
+    errors,
+    isValid,
+  };
+
+  return (
+    <ValidateContext.Provider value={value}>
+      <ValidatePagePresenter />
+    </ValidateContext.Provider>
+  );
 };
--- src/components/pages/ValidatePage/index.ts
+++ src/components/pages/ValidatePage/index.ts
@@ -1,3 +1,23 @@
+import {
+  Control,
+  DeepMap,
+  FieldError,
+  UseFormHandleSubmit,
+  UseFormRegister,
+} from 'react-hook-form';
 import { ValidatePageContainer } from '@pages/ValidatePage/ValidatePageContainer';
 
 export default ValidatePageContainer;
+
+export interface IValidateContext {
+  register: UseFormRegister<ValidateFormInput>;
+  handleSubmit: UseFormHandleSubmit<ValidateFormInput>;
+  control: Control<ValidateFormInput>;
+  onValid: () => void;
+  errors: DeepMap<ValidateFormInput, FieldError>;
+  isValid: boolean;
+}
+
+export interface ValidateFormInput {
+  password: string;
+}
