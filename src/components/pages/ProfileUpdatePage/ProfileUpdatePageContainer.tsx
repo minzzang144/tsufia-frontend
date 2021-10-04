826bfcc78dcbf0beae46f86a4c2f416597a0f003
MinChan Lee <shigatsu970704@gmail.com>
2021-10-04 17:28:07 +0900

    [Add] Profile Update Context

--- src/components/pages/ProfileUpdatePage/ProfileUpdatePageContainer.tsx
+++ src/components/pages/ProfileUpdatePage/ProfileUpdatePageContainer.tsx
@@ -1,7 +1,60 @@
-import React from 'react';
+import { yupResolver } from '@hookform/resolvers/yup';
+import React, { createContext, useContext } from 'react';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+
+import * as I from '.';
 
 import { ProfileUpdatePagePresenter } from '@pages/ProfileUpdatePage/ProfileUpdatePagePresenter';
 
+// Validate Context 생성
+const ProfileUpdateContext = createContext<I.IProfileUpdateContext | undefined>(undefined);
+
+// Validate Context Hook
+export const useProfileUpdateContext = () => {
+  const context = useContext(ProfileUpdateContext);
+  if (!context) throw new Error('Validate Context가 존재하지 않습니다');
+  return context;
+};
+
+const profileUpdateSchema = yup.object().shape({
+  firstName: yup.string().required(),
+  lastName: yup.string().required(),
+  password: yup.string().required(),
+  checkPassword: yup.string().required(),
+});
+
 export const ProfileUpdatePageContainer: React.FC = () => {
-  return <ProfileUpdatePagePresenter />;
+  const {
+    register,
+    handleSubmit,
+    control,
+    getValues,
+    formState: { errors, isValid },
+    reset,
+  } = useForm<I.ProfileUpdateFormInput>({
+    mode: 'all',
+    resolver: yupResolver(profileUpdateSchema),
+  });
+
+  function onValid() {
+    const values = getValues();
+    console.log(values);
+    reset({ firstName: '', lastName: '', password: '', checkPassword: '' });
+  }
+
+  const value = {
+    register,
+    handleSubmit,
+    control,
+    onValid,
+    errors,
+    isValid,
+  };
+
+  return (
+    <ProfileUpdateContext.Provider value={value}>
+      <ProfileUpdatePagePresenter />
+    </ProfileUpdateContext.Provider>
+  );
 };
--- src/components/pages/ProfileUpdatePage/index.ts
+++ src/components/pages/ProfileUpdatePage/index.ts
@@ -1,3 +1,27 @@
+import {
+  Control,
+  DeepMap,
+  FieldError,
+  UseFormHandleSubmit,
+  UseFormRegister,
+} from 'react-hook-form';
+
 import { ProfileUpdatePageContainer } from '@pages/ProfileUpdatePage/ProfileUpdatePageContainer';
 
 export default ProfileUpdatePageContainer;
+
+export interface IProfileUpdateContext {
+  register: UseFormRegister<ProfileUpdateFormInput>;
+  handleSubmit: UseFormHandleSubmit<ProfileUpdateFormInput>;
+  control: Control<ProfileUpdateFormInput>;
+  onValid: () => void;
+  errors: DeepMap<ProfileUpdateFormInput, FieldError>;
+  isValid: boolean;
+}
+
+export interface ProfileUpdateFormInput {
+  firstName: string;
+  lastName: string;
+  password: string;
+  checkPassword: string;
+}
