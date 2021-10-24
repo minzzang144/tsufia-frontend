a6abfa629f66a1a65afd9314ac027790d4d9322e
MinChan Lee <shigatsu970704@gmail.com>
2021-10-24 19:06:09 +0900

    [Add] Contact Page Context on Contact Page Component

--- src/components/pages/ContactPage/ContactPageContainer.tsx
+++ src/components/pages/ContactPage/ContactPageContainer.tsx
@@ -1,7 +1,54 @@
-import React from 'react';
+import { yupResolver } from '@hookform/resolvers/yup';
+import React, { createContext, useContext } from 'react';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+
+import * as I from '.';
 
 import { ContactPagePresenter } from '@pages/ContactPage/ContactPagePresenter';
 
+// Contact Context 생성
+const ContactPageContext = createContext<I.IContactPageContext | undefined>(undefined);
+export const useContactPageContext = () => {
+  const context = useContext(ContactPageContext);
+  if (!context) throw new Error('Contact Page Context가 존재하지 않습니다');
+  return context;
+};
+
+// Contact Validate Schema
+const contactSchema = yup.object().shape({
+  email: yup.string().email().required(),
+  subject: yup.string().required(),
+  message: yup.string().required(),
+});
+
 export const ContactPageContainer: React.FC = () => {
-  return <ContactPagePresenter />;
+  const {
+    register,
+    handleSubmit,
+    control,
+    getValues,
+    formState: { errors, isValid },
+    reset,
+  } = useForm<I.ContactInput>({ mode: 'all', resolver: yupResolver(contactSchema) });
+
+  const onValid = () => {
+    console.log(getValues());
+    reset({ email: '', subject: '', message: '' });
+  };
+
+  const contactValues = {
+    register,
+    handleSubmit,
+    control,
+    onValid,
+    errors,
+    isValid,
+  };
+
+  return (
+    <ContactPageContext.Provider value={contactValues}>
+      <ContactPagePresenter />
+    </ContactPageContext.Provider>
+  );
 };
--- src/components/pages/ContactPage/index.ts
+++ src/components/pages/ContactPage/index.ts
@@ -1,3 +1,26 @@
 import { ContactPageContainer } from '@pages/ContactPage/ContactPageContainer';
+import {
+  Control,
+  DeepMap,
+  FieldError,
+  UseFormHandleSubmit,
+  UseFormRegister,
+} from 'react-hook-form';
 
 export default ContactPageContainer;
+
+// Contact Page Context 인터페이스
+export interface IContactPageContext {
+  register: UseFormRegister<ContactInput>;
+  handleSubmit: UseFormHandleSubmit<ContactInput>;
+  control: Control<ContactInput>;
+  onValid: () => void;
+  errors: DeepMap<ContactInput, FieldError>;
+  isValid: boolean;
+}
+
+export interface ContactInput {
+  email: string;
+  subject: string;
+  message: string;
+}
