41f24afd0e9136bd47d0899d85c6f24356a44631
MinChan Lee <shigatsu970704@gmail.com>
2021-10-27 16:49:27 +0900

    [Deploy] Socket URI

--- src/socket.ts
+++ src/socket.ts
@@ -1,5 +1,8 @@
 import io from 'socket.io-client';
 
-const socket = io('ws://localhost:4000', { transports: ['websocket'], withCredentials: true });
+const socket = io('wss://tsufia-backend.herokuapp.com/', {
+  transports: ['websocket'],
+  withCredentials: true,
+});
 
 export default socket;
